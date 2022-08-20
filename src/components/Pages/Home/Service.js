98263239f08b02ff00e33eebfc5e83fbd441cb41
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-20 23:35:57 +0600

    servic load from mongoDb

--- src/components/Pages/Home/Service.js
+++ src/components/Pages/Home/Service.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Service = ({ img }) => {
+const Service = ({ service }) => {
   return (
     <div>
       <div class="card w-80 glass">
         <figure className=" bg-white">
-          <img src={img} className=" w-[500px] h-[200px] " alt="car!" />
+          <img
+            src={service.imgUrl}
+            className=" w-[500px] h-[200px] "
+            alt="car!"
+          />
         </figure>
         <div class=" px-4 py-2">
-          <h2 class="text-lg font-semibold"> Front End Web Development</h2>
+          <h2 class="text-lg font-semibold"> {service.serviceName}</h2>
           <p className="my-2">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
             Consequatur nobis atque laborum eveniet reiciendis laboriosam?
--- src/components/Pages/Home/Services.js
+++ src/components/Pages/Home/Services.js
@@ -1,20 +1,25 @@
-import React from "react";
-import Service from "./Service";
-import android from "../../../images/android.png";
-import backend from "../../../images/Backend.png";
-import backend2 from "../../../images/backend_1.jpg";
-import frontend from "../../../images/frontEnd.jpg";
-import mern from "../../../images/MERN.png";
-import responsiv from "../../../images/responsiv.jpg";
-import webdesign from "../../../images/webdesign.jpg";
-import android2 from "../../../images/Android-app-development-min.jpg";
+import React, { useEffect, useState } from "react";
 const Services = () => {
-  const array = [webdesign, frontend, backend2, mern, responsiv, android2];
+  const [services, setService] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/allService", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setService(data);
+      });
+  }, []);
 
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-8">
-      {array.map((arr) => (
-        <Service img={arr}></Service>
+      {services?.map((service) => (
+        <Services service={service}></Services>
       ))}
     </div>
   );
