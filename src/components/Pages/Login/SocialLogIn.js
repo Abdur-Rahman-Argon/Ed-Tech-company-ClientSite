af546eb8a40cb67fdf3a3b2f3c72844ba4e2722f
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-20 14:20:59 +0600

    social login

--- src/components/Pages/Login/SocialLogIn.js
+++ src/components/Pages/Login/SocialLogIn.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const SocialLogIn = () => {
-  return <div></div>;
+  const [signInWithGoogle, gUser, loading, error] = useSignInWithGoogle(auth);
+
+  const handleGoogleSignUp = async () => {
+    signInWithGoogle();
+    // await console.log(gUser.user);
+    const displayName = await gUser.user.displayName;
+    const email = await gUser.user.email;
+    const photoURL = await gUser.user.photoURL;
+
+    const user = {
+      displayName,
+      email,
+      photoURL,
+    };
+
+    // console.log(user);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handleGoogleSignUp}
+        className="btn w-full my-2 flex items-center justify-center gap-3 bg-indigo-600 text-white"
+      >
+        SignUp with GooGle
+      </button>
+      <button className="btn w-full my-2 flex items-center justify-center gap-3 bg-indigo-600 text-white">
+        <i class="fa-brands fa-facebook-square text-2xl"></i> SignUp with
+        Facebook
+      </button>
+    </div>
+  );
 };
 
 export default SocialLogIn;
