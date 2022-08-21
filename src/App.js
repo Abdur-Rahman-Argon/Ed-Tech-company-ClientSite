8003601bfe214e00a3ff8393fe9ee55d66d69aa0
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-21 08:12:40 +0600

    route requared

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Shared/Footer";
 import Navbar from "./components/Shared/Navbar";
+import RequireAuth from "./components/Shared/RequireAuth";
 import Home from "./components/Pages/Home/Home";
 import Login from "./components/Pages/Login/Login";
 import SignUp from "./components/Pages/Login/SignUp";
@@ -21,12 +22,23 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<SignUp></SignUp>}></Route>
-        <Route path="/reviewAdd" element={<ReviewAdd></ReviewAdd>}></Route>
+        <Route
+          path="/reviewAdd"
+          element={
+            <RequireAuth>
+              <ReviewAdd></ReviewAdd>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/contactUs" element={<ContactUs></ContactUs>}></Route>
         <Route path="/services" element={<Services></Services>}></Route>
         <Route
           path="/serviceDetails"
-          element={<ServiceDetails></ServiceDetails>}
+          element={
+            <RequireAuth>
+              <ServiceDetails></ServiceDetails>
+            </RequireAuth>
+          }
         ></Route>
       </Routes>
 
